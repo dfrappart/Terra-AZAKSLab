48f639bbefef882b039c70b08d13bf09afdf5cb4
dfrappart <david@frappart.com>
2019-05-20 10:18:38 +0200

    adding netpol for allow in inside a namespace

--- 01 AKS Clusters/06 Kube NetPol.tf
+++ 01 AKS Clusters/06 Kube NetPol.tf
@@ -1,6 +1,6 @@
 ##################################################################
 # Network policy
-/*
+
 #Default network policy deny all in namespace terra-test-namespace ingress
 
 resource "kubernetes_network_policy" "terra_defaultnp_denyallin_ns_terra-test-namespace" {
@@ -20,6 +20,35 @@ resource "kubernetes_network_policy" "terra_defaultnp_denyallin_ns_terra-test-na
 
 
 }
+
+#Default network policy deny all ingress traffic from other namespace tp pods in  terra-test-namespace
+
+resource "kubernetes_network_policy" "terra_defaultnp_denyallin_fromotherns" {
+  metadata {
+    name        = "defaultnp_denyallin_fromotherns"
+    namespace   = "${kubernetes_namespace.terra_test_namespace.metadata.0.name}"
+  }
+
+  spec {
+    pod_selector {
+      match_labels {}
+
+    }
+    ingress = [
+      {
+        from = [
+          {
+            pod_selector {}
+          }
+          
+        ]
+      }
+    ]
+    policy_types = ["Ingress"]
+  }
+}
+
+
 
 #Default network policy deny all in namespace terra-test-namespace egress
 /*
@@ -44,7 +73,7 @@ resource "kubernetes_network_policy" "terra_defaultnp_denyalleg_ns_terra-test-na
 */
 
 #Network policy allowing external traffic on testnginxpod
-/*
+
 resource "kubernetes_network_policy" "Allow-External" {
   metadata {
     name = "allow-external"
@@ -67,98 +96,5 @@ resource "kubernetes_network_policy" "Allow-External" {
 
   }
 }
-*/
-
-#Network policy allowing external traffic on Azure app
-/*
-resource "kubernetes_network_policy" "Allow-External-AzApp" {
-  metadata {
-    name = "allow-external-azapp"
-    namespace = "terra-test-namespace"
-
-  }
-
-  spec {
-    pod_selector {
-      match_labels {
-        app = "azure-vote-front"
-      }
-    }
-    ingress = [
-      {
-        from = []
-      }
-    ]
-    policy_types = ["Ingress"]
-
-  }
-}
-*/
-
-#Network policy allowing Egress traffic on Azure app front to azure app back
-/*
-resource "kubernetes_network_policy" "Allow-Egress-AzAppFront" {
-  metadata {
-    name = "allow-egress-azappfront"
-    namespace = "terra-test-namespace"
-
-  }
-
-  spec {
-    pod_selector {
-      match_labels {
-        app = "azure-vote-front"
-      }
-    }
-    egress = [
-      {
-        to = {
-          pod_selector {
-            match_labels {
-              app = "azure-vote-back"
-            }
-          }
-        }
-      }
-    ]
-    policy_types = ["Egress"]
-
-  }
-}
-*/
-#Network policy allowing ingress traffic from Azure app front to azure app back
-/*
-resource "kubernetes_network_policy" "Allow-Ingress-AzAppBack" {
-  metadata {
-    name = "allow-ingress-azappback"
-    namespace = "terra-test-namespace"
-
-  }
-
-  spec {
-    pod_selector {
-      match_labels {
-        app = "azure-vote-back"
-      }
-    }
-    ingress = [
-      {
-        from = [
-          {
-            pod_selector {
-              match_labels {
-                app = "azure-vote-front"
-              }
-            }
-          }
-        ]
-      }
-    ]
-    policy_types = ["Ingress"]
-
-  }
-}
-*/
 
 
-#Network policy Allow traffic from a namespace
